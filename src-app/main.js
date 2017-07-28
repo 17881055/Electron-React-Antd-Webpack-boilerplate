315125013cb8f6723ce2ff84d6b6a8f18aeea9d9
ethan <17881055@qq.com>
2017-07-28 14:01:04 +0800

    更新

--- src-app/main.js
+++ src-app/main.js
@@ -3,28 +3,17 @@ const electron = require('electron');
 const exec = require('child_process').exec;
 var path = require('path');
 const url = require('url');
-
 const app = electron.app;
 const dialog = electron.dialog;
 const menu = electron.Menu;
 
-// Adds debug features like hotkeys for triggering dev tools and reload
-//require('electron-debug')();
-
-// Prevent window being garbage collected
 let mainWindow;
 
-let isDevelopment = true;
-
-if (isDevelopment) {
-	require('electron-reload')(__dirname, {
-		ignored: /node_modules|[\/\\]\./
-	});
-}
+require('electron-reload')(__dirname, {
+	ignored: /node_modules|[\/\\]\./
+});
 
 function onClosed() {
-	// Dereference the window
-	// For multiple windows store them in an array
 	mainWindow = null;
 }
 
@@ -34,6 +23,8 @@ function execute(command, callback) {
 
 function createMainWindow() {
 	const win = new electron.BrowserWindow({
+		backgroundColor: '#2e2c29',
+		resizable: false,
 		width: 1200,
 		height: 800,
 		frame: true //无边框窗口
@@ -45,11 +36,8 @@ function createMainWindow() {
 		slashes: true
 	});
 
-	//win.loadURL(`file://${__dirname}/dist/index.html`);
-	console.log(indexPath);
 	win.loadURL(indexPath);
 	win.on('closed', onClosed);
-
 	return win;
 }
 
@@ -68,9 +56,4 @@ app.on('activate', () => {
 
 app.on('ready', () => {
 	mainWindow = createMainWindow();
-	/* execute('weinre --httpPort 9000 --debug --boundHost 192.168.9.1', function (output) {
-		console.log("ping", output);
-	}); */
-
-
 });
